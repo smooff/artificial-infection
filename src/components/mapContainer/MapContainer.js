f34e8ae7b3b3b8fc64fdd155852207a62bc62f44
Simon <smo.offchannel@gmail.com>
2021-03-22 17:55:51 +0100

    fix: optimalizovanie mapy -zmena vyzoru stranky (vylepsenie rozlozenia komponentov a resize mapy) -priprava na responzivnost

--- src/components/mapContainer/MapContainer.js
+++ src/components/mapContainer/MapContainer.js
@@ -11,6 +11,8 @@ import {
 import {mapContainerState} from "./MapContainerState";
 import {useRecoilState} from "recoil";
 
+import './MapContainer.css';
+
 
 const geoUrl =
     "https://raw.githubusercontent.com/smooff/mapData/main/mapCoordsDataFetch.json";
@@ -49,71 +51,103 @@ const MapContainer = ({setTooltipContent}) => {
         // console.log(mapData);
     }, [mapData]);
 
+    //pracuje s vyskou a sirkou okna, aby bola mapa responz.
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height
+        };
+    }
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+        useEffect(() => {
+            function handleResize() {
+                setWindowDimensions(getWindowDimensions());
+            }
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
+        return windowDimensions;
+    }
+    const { height, width } = useWindowDimensions();
+    //PO TADIALTO
+
+    const mapHeight= height*0.70;
+    const mapWidth= width*0.75;
     return (
-        <ComposableMap
-            projectionConfig={{
-                rotate: [-10, 0, 0],
-                scale: 150
-            }}
-            width={800}
-            height={400}
-            style={{
-                width: "80%",
-                height: "auto",
-                marginLeft: "-15%",
-                backgroundImage: "radial-gradient(skyBlue, cornflowerBlue, royalBlue )"
-            }}
-        >
-            <ZoomableGroup zoom={1}>
-                {/*<Sphere stroke="#E4E5E6" strokeWidth={0.5}/>*/}
-                {/*<Graticule stroke="#E4E5E6" strokeWidth={0.5}/>*/}
-                {data.length > 0 && (
-                    <Geographies geography={geoUrl}>
-                        {({geographies}) =>
-                            geographies.map((geo) => {
-                                const d = mapData[geo.properties.ISO_A3];
-                                return (
-                                    <Geography
-                                        key={geo.rsmKey}
-                                        geography={geo}
-                                        fill={d?.Infectious ? colorScale(d?.Infectious) : "#F5F4F6"}
+        <div >
+            <ComposableMap
+                projectionConfig={{
+                    rotate: [-10, 0, 0],
+                    scale: 250
+                }}
+                width={mapWidth}
+                height={mapHeight}
+                style={{
+                    // width: "100%", height: "100%",
+                    backgroundImage: "radial-gradient(skyBlue, cornflowerBlue, royalBlue )"
+                }}
 
-                                        onMouseEnter={() => {
-                                            const {NAME, POP_EST} = geo.properties;
-                                            // setTooltipContent(d?.Population ? `${d.NAME} — ${rounded(POP_EST)}` : "");
-                                            setTooltipContent(d?.Population ? `${d.NAME} — ${rounded(d.Population)} SUS- ${d.Susceptible} INF- ${d.Infectious} REC- ${d.Recovered} DEC- ${d.Deceased}` : "");
-                                        }}
-                                        onMouseLeave={() => {
-                                            setTooltipContent("");
-                                        }}
-                                        style={{
-                                            default: {
-                                                // fill: "#D6D6DA",
-                                                // outline: "none"
-                                                stroke: "grey",
-                                                strokeOpacity: 0.2
-                                            },
-                                            hover: {
-                                                fill: "#F53",
-                                                outline: "none",
-                                                stroke: "black",
-                                                strokeOpacity: 1
-                                            },
-                                            pressed: {
-                                                fill: "#E42",
-                                                outline: "none"
-                                            }
-                                        }}
-                                    />
-                                );
-                            })
-                        }
-                    </Geographies>
-                )}
-            </ZoomableGroup>
+            >
+                <ZoomableGroup zoom={1}
+                               translateExtent={[
+                                   [0, -mapHeight],
+                                   [mapWidth, mapHeight]
+                               ]}
+                >
+                    {/*<Sphere stroke="#E4E5E6" strokeWidth={0.5}/>*/}
+                    {/*<Graticule stroke="#E4E5E6" strokeWidth={0.5}/>*/}
+                    {data.length > 0 && (
+                        <Geographies geography={geoUrl}>
+                            {({geographies}) =>
+                                geographies.map((geo) => {
+                                    const d = mapData[geo.properties.ISO_A3];
+                                    return (
+                                        <Geography
+                                            key={geo.rsmKey}
+                                            geography={geo}
+                                            fill={d?.Infectious ? colorScale(d?.Infectious) : "#F5F4F6"}
 
-        </ComposableMap>
+                                            // onMouseEnter={() => {
+                                            //     const {NAME, POP_EST} = geo.properties;
+                                            //     // setTooltipContent(d?.Population ? `${d.NAME} — ${rounded(POP_EST)}` : "");
+                                            //     setTooltipContent(d?.Population ? `${d.NAME} — ${rounded(d.Population)} SUS- ${d.Susceptible} INF- ${d.Infectious} REC- ${d.Recovered} DEC- ${d.Deceased}` : "");
+                                            // }}
+                                            // onMouseLeave={() => {
+                                            //     setTooltipContent("");
+                                            // }}
+                                            style={{
+                                                default: {
+                                                    // fill: "#D6D6DA",
+                                                    // outline: "none"
+                                                    stroke: "grey",
+                                                    strokeOpacity: 0.2
+                                                },
+                                                hover: {
+                                                    fill: "#F53",
+                                                    outline: "none",
+                                                    stroke: "black",
+                                                    strokeOpacity: 1
+                                                },
+                                                pressed: {
+                                                    fill: "#E42",
+                                                    outline: "none"
+                                                }
+                                            }}
+                                        />
+                                    );
+                                })
+                            }
+                        </Geographies>
+                    )}
+                </ZoomableGroup>
 
+            </ComposableMap>
+        </div>
 
     );
 };
